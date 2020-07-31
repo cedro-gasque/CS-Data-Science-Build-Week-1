4ab09675246d0443fee6b9b050483985556783ef
Ramses Gasque <cedro.gasque@wsu.edu>
2020-07-31 10:01:58 -0700

    no actual features added, just making sure I don't throw errors in my own code lol

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def distance(pointA, pointB, _norm=np.linalg.norm):
 
 class KNNeighbors:
     def __init__(self, formula=distance, k=1):
-        self.points = np.array([])
         self.formula = distance
         self.k = k
 
     def fit(self, points, classes):
-        self.points = points.values.reshape(-1, points.shape[1])
-        self.classes = classes.values.reshape(-1, 1)
+        try:
+            self.points = points.values.reshape(-1, points.shape[1])
+            self.classes = classes.values.reshape(-1, 1)
+        except:
+            self.points = points.reshape(-1, points.shape[1])
+            self.classes = classes.reshape(-1, 1)
 
     def predict(self, point):
         distances = np.hstack((self.points, self.formula(self.points, point).reshape(-1, 1), self.classes))
